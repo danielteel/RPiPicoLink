1434bb662eb806160593366915306d86605a7c7a
Daniel Teel <daniel.teel.2@us.af.mil>
2023-10-25 23:00:15 -0600

    asd

--- main.py
+++ main.py
@@ -1,145 +1,138 @@
-import time
+import network
+import socket
+import asyncio
+import parse
 
-def isdigit(c):
-    return c>=48 and c<=57
-def isalpha(c):
-    return (c>=97 and c<=122) or (c>=65 and c<=90)
-def isalnum(c):
-    return (c>=97 and c<=122) or (c>=65 and c<=90) or (c>=48 and c<=57)
-def ishex(c):
-    return (c>=48 and c<=57) or (c>=97 and c<=102) or (c>=65 and c<=70)
-
-def tokenize(text):
-    tokens=[]
-    look=text[0]
-    lookIndex=0
-
-    def getChar():
-        nonlocal look, lookIndex
-        lookIndex+=1
-        try:
-            look=text[lookIndex]
-        except:
-            look=b''
-
-    def peek():
-        nonlocal lookIndex
-        peekIndex = lookIndex + 1
-        if peekIndex>=len(text):
-            return b''
-        return text[peekIndex]
-
-    def skipWhite():
-        nonlocal look
-        while look<=32:
-            getChar()
+from machine import ADC
+from machine import Pin
+from machine import Timer
 
 
-    while look:
-        skipWhite()
+led = machine.Pin("LED", machine.Pin.OUT)
+
+def getTemp():
+    adc = ADC(4) 
+    ADC_voltage = adc.read_u16() * (3.3 / 65536.0)
+    temperature_celcius = 27.0 - (ADC_voltage - 0.706)/0.001721
+    return 32.0+1.8*temperature_celcius
+
+
+STAT_IDLE = 0
+STAT_CONNECTING = 1
+STAT_GOT_IP = 3
+STAT_CONNECT_FAIL = -1
+STAT_NO_AP_FOUND = -2
+STAT_WRONGPASSWORD = -3
+
+STATE_NEW = 0
+STATE_LISTENING = 1
+STATE_CONNECTING = 2
+STATE_CONNECTED = 3
+STATE_PEER_CLOSED = 4
+STATE_ACTIVE_UDP = 5
+
+
+    #define STATE_NEW 0
+    #define STATE_LISTENING 1
+    #define STATE_CONNECTING 2
+    #define STATE_CONNECTED 3
+    #define STATE_PEER_CLOSED 4
+    #define STATE_ACTIVE_UDP 5
+    # 0,                /* ERR_OK          0      No error, everything OK  */
+    # MP_ENOMEM,        /* ERR_MEM        -1      Out of memory error      */
+    # MP_ENOBUFS,       /* ERR_BUF        -2      Buffer error             */
+    # MP_EWOULDBLOCK,   /* ERR_TIMEOUT    -3      Timeout                  */
+    # MP_EHOSTUNREACH,  /* ERR_RTE        -4      Routing problem          */
+    # MP_EINPROGRESS,   /* ERR_INPROGRESS -5      Operation in progress    */
+    # MP_EINVAL,        /* ERR_VAL        -6      Illegal value            */
+    # MP_EWOULDBLOCK,   /* ERR_WOULDBLOCK -7      Operation would block    */
+    # MP_EADDRINUSE,    /* ERR_USE        -8      Address in use           */
+    # MP_EALREADY,      /* ERR_ALREADY    -9      Already connecting       */
+    # MP_EALREADY,      /* ERR_ISCONN     -10     Conn already established */
+    # MP_ENOTCONN,      /* ERR_CONN       -11     Not connected            */
+    # -1,               /* ERR_IF         -12     Low-level netif error    */
+    # MP_ECONNABORTED,  /* ERR_ABRT       -13     Connection aborted       */
+    # MP_ECONNRESET,    /* ERR_RST        -14     Connection reset         */
+    # MP_ENOTCONN,      /* ERR_CLSD       -15     Connection closed        */
+    # MP_EIO,           /* ERR_ARG        -16     Illegal argument.        */
+    # MP_EBADF,         /* _ERR_BADF      -17     Closed socket (null pcb) */
+
+class SockError(Exception):
+    pass
+
+wlan=None
+async def connect(ssid, password, ipAddress, port):
+    global wlan
+    wlanConnectTime=0
+    wlanConnectTimeout=10
+    wlan=network.WLAN(network.STA_IF)
+    wlan.disconnect()
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    sock = None
+    try:
+        sockAddress = socket.getaddrinfo(ipAddress, port)[0][-1]
+    except OSError:
+        return
+
+    while 1:
+        while wlan.status()!=STAT_GOT_IP:
+            led.toggle()
+            if wlanConnectTime>wlanConnectTimeout:
+                wlan.disconnect()
+                wlan.connect(ssid, password)
+                wlanConnectTime=0
+            print(b'DanNet: trying to establish WiFi connection', wlanConnectTime)
+            await asyncio.sleep(0.5)
+            wlanConnectTime+=0.5
+
+        wlanConnectTime=0
         
-        if not look:
-            break
+        while wlan.status()==STAT_GOT_IP:
 
-        if isalpha(look):#identifiers
-            startIndex=lookIndex
-            while isalnum(look):
-                getChar()
-                if not isalnum(look):                    
-                    tokens.append({b't':b'n', b'v': text[startIndex:lookIndex]})
-
-        elif look==48 and (peek()==120 or peek()==88):#hex int
-            getChar()
-            getChar()
-            startIndex=lookIndex
-            if not ishex(look):
-                raise ValueError('bad hex literal')
-            while ishex(look):
-                getChar()
-                
-            tokens.append({b't':b'i', b'v': int(text[startIndex:lookIndex], 16)})
-
-        elif look==b'-' or look==b'+' or look==b'.' or isdigit(look):#ints, floats, or + - .
-            unary=b''
-            if look==b'-' or look==b'+':
-                unary=look
-                getChar()
-                skipWhite()
-            if not look or not (look==b'.' or isdigit(look)):
-                tokens.append({b't':b's', b'v': unary})
-            else:
-                startIndex=lookIndex
-                hasDec=False
-                notDone=True
-                while look and notDone:
-                    notDone=False
-                    if isdigit(look):
-                        notDone=True
-                    elif look==b'.' and not hasDec:
-                        hasDec=True
-                        notDone=True
-                    if notDone:
-                        getChar()
-                if lookIndex-startIndex==1 and text[startIndex]==b'.':
-                    if unary:
-                        tokens.append({b't':b's', b'v': unary})
-                    tokens.append({b't':b's', b'v': b'.'})
-                else:
-                    if hasDec:
-                        val=float(text[startIndex:lookIndex])
-                        if unary==b'-':
-                            val=-val
-                        tokens.append({b't':b'f', b'v': val})
-                    else:
-                        val=int(text[startIndex:lookIndex])
-                        if unary==b'-':
-                            val=-val
-                        tokens.append({b't':b'i', b'v': val})
+            sock=socket.socket()   
             
-        elif look==b'"' or look==b"'":#strings
-            toMatch = look
-            getChar()
-            startIndex=lookIndex
-            while look and look!=toMatch:
-                getChar()
-            if look!=toMatch:
-                raise ValueError('bad string literal')
-            else:
-                tokens.append({b't':b'$', b'v': text[startIndex:lookIndex]})
-                getChar()
+            await asyncio.sleep_ms(250)
+            try:
+                print("DanNet: trying to connect socket")
+                led.toggle()
+                try:
+                    sock.connect(sockAddress)
+                except OSError as err:
+                    if err.errno!=104:#ECONNRESET
+                        raise err
+                while wlan.status()==STAT_GOT_IP and str(sock)[:15].encode()==b'<socket state=3':
+                    
+                    led.toggle()
 
-        else:#symbols
-            tokens.append({b't':b's', b'v': look})
-            getChar()
-    return tokens
+                    recvData=b''
+                    while str(sock)[:15].encode()==b'<socket state=3' and ('incoming=0 ' not in str(sock)):
+                        byte=sock.recv(1)
+                        if byte==b'\n':
+                            parse.compile(recvData)
+                            recvData=b''
+                        else:
+                            recvData = recvData + byte
+
+                    sock.write(('<info temp='+str(getTemp())+'>'+'\n').encode())
+                    await asyncio.sleep_ms(0)
+
+                raise SockError('connection lost')
+
+            except SockError as err:
+                print("DanNet:", err)
+                sock.close()
+                sock=None
 
 
-def buildDictionary(tokens):
-    dict = {}
-    lastIdent=None
-    for token in tokens:
-        if token[b't']==b'n':
-            lastIdent=token[b'v']
-        elif token[b't']==b's':
-            if token[b'v']!=ord(b'='):
-                lastIdent=None
-        elif lastIdent and (token[b't']=='$' or token[b't']==b'i' or token[b't']==b'f'):
-            dict[lastIdent]=token[b'v']
-            lastIdent=None
-        else:
-            lastIdent=None
-    return dict
+async def main():
+    led.value(1)
+    while 1:
+        await connect('Pico', '123456789', '10.42.0.1', 80)
 
-#types
-#s - symbol
-#n - name
-#i - int
-#f - float
-#$ - string
 
-startTime = time.ticks_ms()
-tokens = tokenize(b'state=3 timeout=-1 incoming=0x2000fc0c off=0 data="asdflkjasdf asdfkj asd fjakjsadf asjf asldf jasflkjadf askdjf asd0fas0df 98asd0f98 as9fd 8asdf9 8asd0f98 as09f 8asd f8a sa9 df8a0s9f 8as90f8 asdfl jkasdf as09f 8as09f8 asdf9 8as09f 8as0df9 8asd0f 98as09fd 8as09f8 asf 8asdf as asd fasdf 908as09f8as 98fa sdf08asd f09asdf asdjf l3 345j3k4h5 23kj5hjkdfkjgh sfgkj hsfgkjhsfgksdfgsfg8s dfg8sfgs8dfg sdfg7 sdf9gs98fg6 fasdflkjasdf asdfkj asd fjakjsadf asjf asldf jasflkjadf askdjf asd0fas0df 98asd0f98 as9fd 8asdf9 8asd0f98 as09f 8asd f8a sa9 df8a0s9f 8as90f8 asdfl jkasdf as09f 8as09f8 asdf9 8as09f 8as0df9 8asd0f 98as09fd 8as09f8 asf 8asdf as asd fasdf 908as09f8as 98fa sdf08asd f09asdf asdjf l3 345j3k4h5 23kj5hjkdfkjgh sfgkj hsfgkjhsfgksdfgsfg8s dfg8sfgs8dfg sdfg7 sdf9gs98fg6 f"')
-dict=buildDictionary(tokens)
-endTime = time.ticks_ms()
-print(dict)
-print(endTime-startTime)
+try:
+    asyncio.run(main())
+finally:
+    asyncio.new_event_loop()
---
+++ parse.py
@@ -0,0 +1,145 @@
+import re
+
+class ParseError(Exception):
+    pass
+
+def tokenize(string):
+    if type(string) is str:
+        string=string.encode()
+    index=0
+    number=re.compile(rb"([-+]?((\d+\.?\d*)|(\d*\.?\d+)))")
+    ident=re.compile(rb"([a-zA-Z]+\d*[a-zA-Z0-9_]*)")
+    symbol=re.compile(rb'([=\[\],;<>])')
+    tokens=[]
+    stringLen = len(string)
+    while index<stringLen:
+
+        if string[index]<=32:
+            start=index+1
+            index+=1
+            while index<stringLen and string[index]<=32:
+                index+=1
+            tokens.append((b'white',))
+            if index>=stringLen:
+                break
+
+        if string[index]==b'"'[0] or string[index]==b"'"[0]:
+            stringStart=string[index]
+            start=index+1
+            index+=1
+            if index>=stringLen:
+                raise ParseError('Tokenize: failed to tokenize, unterminated string at index '+str(start)+":"+str(index))
+            while string[index]!=stringStart:
+                index+=1
+                if index>=stringLen:
+                    raise ParseError('Tokenize: failed to tokenize, unterminated string at index '+str(start)+":"+str(index))
+            tokens.append((b'string', string[start:index]))
+            index+=1
+            continue
+
+        match=number.match(string[index:])
+        if match:
+            num = match.group(1)
+            if b'.' in num:
+                tokens.append((b'number', float(num)))
+            else:
+                tokens.append((b'number', int(num)))
+            index+=match.end(0)
+            continue
+
+        match=ident.match(string[index:])
+        if match:
+            tokens.append((b'name', match.group(1)))
+            index+=match.end(0)
+            continue
+
+        match=symbol.match(string[index:])
+        if match:
+            tokens.append((b'symbol', match.group(1)))
+            index+=match.end(0)
+            continue
+
+        break
+    if index!=stringLen:
+        raise ParseError('Tokenize: unexpected character at index: '+str(index)+', char: '+chr(string[index]))
+
+    return tokens
+
+def parse(inTokens):
+    tokens=[]
+    lastToken=None
+    for token in inTokens:
+        if lastToken and lastToken[0]==b'number' and token[0]==b'name':
+            raise ParseError('invalid syntax, number:'+str(lastToken[1])+' followed immediately by name:'+str(token[1]))
+        lastToken=token
+        if token[0]!=b'white':
+            tokens.append(token)
+    tokens.append((b'end',))
+    token=None
+    tokenIndex = 0
+    tokenEndIndex=len(tokens)
+
+    def getToken():
+        nonlocal tokenIndex, token, tokenEndIndex
+        if tokenIndex<tokenEndIndex:
+            token=tokens[tokenIndex]
+            tokenIndex+=1
+        else:
+            token=None
+
+
+    def matchSymbol(value=None):
+        nonlocal token
+        if token and token[0]==b'symbol':
+            if value and token[1]!=value:
+                raise ParseError('expected value of '+str(value)+' but found '+str(token[1]))
+            getToken()
+        else:
+            raise ParseError('expected type of symbol but found '+(str(token[0]) if token else 'nothing'))
+        
+    def matchEnd():
+        nonlocal token
+        if not (token and token[0]==b'end'):
+            raise ParseError('expected end of token but found '+(str(token[0]) if token else 'nothing'))
+        
+    def matchName():
+        nonlocal token
+        if token and token[0]==b'name':
+            name=token[1]
+            getToken()
+            return name
+        else:
+            raise ParseError('expected type of name but found '+(str(token[0]) if token else 'nothing'))
+        
+    def matchValue():
+        nonlocal token
+        if token and token[0] in [b'name', b'string', b'number']:
+            value=token[1]
+            getToken()
+            return value
+        else:
+            raise ParseError('expected type of name but found '+(str(token[0]) if token else 'nothing'))
+
+    getToken()
+    matchSymbol(b'<')
+    objName=matchName()
+    props={}
+    while token and token[0] in [b'name']:
+        prop=matchName()
+        matchSymbol(b'=')
+        value=matchValue()
+        props[prop]=value
+    matchSymbol(b'>')
+    matchEnd()
+
+    return objName,props
+
+def compile(text):
+    try:
+        tokens=tokenize(text)
+        obj=parse(tokens)
+        return obj
+    except ParseError as err:
+        print("ParseError: ", err)
+        return None
+    
